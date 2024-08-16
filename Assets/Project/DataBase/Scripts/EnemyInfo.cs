952d357e38c510539679be15c0d65733d549e01b
doguto <doguto110@gmail.com>
2024-08-16 14:42:25 +0900

    [fix] DataBase directory

---
+++ Assets/Project/DataBase/Scripts/EnemyInfo.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class EnemyInfo
+{
+    public GameObject enemy;
+    public int cost;
+}
---
+++ Assets/Project/DataBase/Scripts/SpawnInfo.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[System.Serializable]
+public class SpawnInfo
+{
+    public int waveAmount;
+    public float waveSeconds;
+    public int enemyUpper;
+    public int cost;
+}
